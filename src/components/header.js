c859e5e64ed318e8e15be339ad29a0b231ad9518
Krinner, Roland <roland-krinner@hotmail.com>
2020-05-05 21:27:18 +0200

    navigation svg

--- src/components/header.js
+++ src/components/header.js
@@ -91,29 +91,48 @@ const NavContent = ({ pageInfo }) => {
 
 export default ({ pageInfo }) => {
 	return (
-		<header>
-			{/* <Navbar bg="light" expand="lg" variant="light" collapseOnSelect className={Styles.navbar}>
-				<NavContent pageInfo={pageInfo} />
-			</Navbar> */}
-			{/* <div>Sandra Läuger - Beckenbodentraining</div> */}
-			{/* <Navbar bg="brown-1" expand="lg" variant="dark" collapseOnSelect className={Styles.navbar}>
+		<header className={`mb-n11 mb-xl-n12 ${Styles.header}`}>
+			<Navbar expand="lg" variant="dark" collapseOnSelect className={Styles.navbar}>
 				<NavContent pageInfo={pageInfo} />
 			</Navbar>
-			<Navbar bg="brown-2" expand="lg" variant="dark" collapseOnSelect className={Styles.navbar}>
-				<NavContent pageInfo={pageInfo} />
-			</Navbar> */}
-			<Navbar bg="brown-2" expand="lg" variant="dark" collapseOnSelect className={Styles.navbar}>
-				<NavContent pageInfo={pageInfo} />
-			</Navbar>
-			{/* <Navbar bg="brown-4" expand="lg" variant="dark" collapseOnSelect className={Styles.navbar}>
-				<NavContent pageInfo={pageInfo} />
-			</Navbar>
-			<Navbar bg="brown-5" expand="lg" variant="dark" collapseOnSelect className={Styles.navbar}>
-				<NavContent pageInfo={pageInfo} />
-			</Navbar>
-			<Navbar bg="brown-6" expand="lg" variant="dark" collapseOnSelect className={Styles.navbar}>
-				<NavContent pageInfo={pageInfo} />
-			</Navbar> */}
+			<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewBox="0 0 1920 400">
+				<defs>
+					<linearGradient id="gradient" gradientTransform="rotate(55)">
+						<stop stopColor="#dc3545" offset="0%"></stop>
+						<stop stopColor="#fd7e14" offset="35%"></stop>
+						<stop stopColor="#fad961" offset="100%"></stop>
+					</linearGradient>
+				</defs>
+				<mask id="mask">
+					<rect width="1920" height="400" fill="white"></rect>
+					<path d="M1920 4.719v69.5c-362.63 60.036-692.797 55.536-990.5-13.5C565.833-23.615 256 12.813 0 170L1 4.719h1919z" fill="black" transform="translate(0,230), rotate(180 960.5 87.36)"></path>
+				</mask>
+				<g fill="#fff" mask="url(#mask)">
+					<rect width="1920" height="400" fill="url(#gradient)"></rect>
+					<path d="M1 170V99c269.033-70.44 603.533-66.44 1003.5 12C1494 207 1921 4.719 1921 4.719L1920 170H1z" fillOpacity=".3" fill="#FFF" transform="translate(0, 230)"></path>
+					<path d="M1 170.75V99C373.115 4.216 705.281-4.951 997.5 71.5c365.667 95.667 673.5 73.406 923.5-66.781l-1 166.031H1z" fillOpacity=".3" fill="#FFF" transform="translate(0, 230)"></path>
+					<path d="M1 170v-67C400.333-1.333 744.167-19 1032.5 50c432.5 103.5 754 19.219 888.5-45.281l-1 166.031L1 170z" fillOpacity=".35" fill="#FFF" transform="translate(0, 230)"></path>
+				</g>
+			</svg>
+			{/* <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewBox="0 0 1920 570">
+				<defs>
+					<linearGradient id="gradient" gradientTransform="rotate(55)">
+						<stop stopColor="#dc3545" offset="0%"></stop>
+						<stop stopColor="#fd7e14" offset="35%"></stop>
+						<stop stopColor="#fad961" offset="100%"></stop>
+					</linearGradient>
+				</defs>
+				<mask id="mask">
+					<rect width="1920" height="600" fill="white"></rect>
+					<path d="M1920 4.719v69.5c-362.63 60.036-692.797 55.536-990.5-13.5C565.833-23.615 256 12.813 0 170L1 4.719h1919z" fill="black" transform="translate(0,400), rotate(180 960.5 87.36)"></path>
+				</mask>
+				<g fill="#fff" mask="url(#mask)">
+					<rect width="1920" height="600" fill="url(#gradient)"></rect>
+					<path d="M1 170V99c269.033-70.44 603.533-66.44 1003.5 12C1494 207 1921 4.719 1921 4.719L1920 170H1z" fillOpacity=".3" fill="#FFF" transform="translate(0, 400)"></path>
+					<path d="M1 170.75V99C373.115 4.216 705.281-4.951 997.5 71.5c365.667 95.667 673.5 73.406 923.5-66.781l-1 166.031H1z" fillOpacity=".3" fill="#FFF" transform="translate(0, 400)"></path>
+					<path d="M1 170v-67C400.333-1.333 744.167-19 1032.5 50c432.5 103.5 754 19.219 888.5-45.281l-1 166.031L1 170z" fillOpacity=".35" fill="#FFF" transform="translate(0, 400)"></path>
+				</g>
+			</svg> */}
 		</header>
 	)
 }
